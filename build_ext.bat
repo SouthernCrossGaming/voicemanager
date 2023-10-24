af71bf24344907764bcc90bfb19e8a4e34083280
Fraeven <maxwilliamgrimm@gmail.com>
2023-10-24 19:37:28 -0400

    working build

--- build_ext.bat
+++ build_ext.bat
@@ -6,5 +6,5 @@ rmdir .\build /s /q
 docker compose build
 docker compose run extension-build --remove-orphans
 
-echo f | XCOPY build\voicemanager.ext.2.sdk2013\voicemanager.ext.2.sdk2013.so ..\addons\sourcemod\extensions\voicemanager.ext.2.sdk2013.so
-echo f | XCOPY build\voicemanager.ext.2.tf2\voicemanager.ext.2.tf2.so ..\addons\sourcemod\extensions\voicemanager.ext.2.tf2.so
+echo f | XCOPY build\voicemanager.ext.2.sdk2013\voicemanager.ext.2.sdk2013.so ..\addons\sourcemod\extensions\voicemanager.ext.2.sdk2013.so /Y
+echo f | XCOPY build\voicemanager.ext.2.tf2\voicemanager.ext.2.tf2.so ..\addons\sourcemod\extensions\voicemanager.ext.2.tf2.so /Y
--- extension/scripts/build.bat
+++ extension/scripts/build.bat
@@ -1,8 +1,4 @@
-COPY C:\extension\scripts\setup.py C:\ambuild\setup.py
-pip install C:\ambuild
-
 mkdir build
 cd build
-
 python ..\configure.py -s tf2,sdk2013 --sm-path C:\sourcemod --mms-path C:\metamod-source --hl2sdk-root C:\sdks --enable-optimize
 ambuild
